8bb355e0a5cee8b6e8152311950025709e557034
Giada <21024993+GiadaSimonetti@users.noreply.github.com>
2019-11-30 13:23:12 +0000

    WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
-import "./App.css";
+import styled from "styled-components";
+import Cloud from "./Cloud/Cloud";
+
+const Wrapper = styled.div`
+   {
+    min-height: 100vh;
+    text-align: center;
+    background-color: #87cefa;
+  }
+`;
+
+const Title = styled.h1`
+   {
+    font-size: calc(10px + 2vmin);
+    color: white;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Wrapper>
+      <Title className="App-header">Flappy something</Title>
+      <Cloud />
+    </Wrapper>
   );
 }
 
---
+++ src/Bird/Bird.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "react-dom";
+import ReactSVG from "react-svg";
+
+function Cloud() {
+  render(<ReactSVG src="svg.svg" />);
+}
+
+export default Cloud;
