f337cb3ca1a6d52e7864173168991f1fcd0ba455
Giada <21024993+GiadaSimonetti@users.noreply.github.com>
2020-02-07 16:43:31 +0000

    Clean up

--- src/Cloud/Cloud.js
+++ src/Cloud/Cloud.js
@@ -1,42 +1,40 @@
 import React from "react";
 import cloud1 from "./cloud1.svg";
-// import cloud2 from "./cloud2.svg";
+import cloud2 from "./cloud2.svg";
 import cloud3 from "./cloud3.svg";
-import cloud4 from "./cloud4.svg";
-// import cloud6 from "./cloud6.svg";
-// import cloud7 from "./cloud7.svg";
-// import cloud8 from "./cloud8.svg";
-import { CloudContainer, Cloud3, Cloud4, Cloud1 } from "./styles";
+import { CloudContainer, Cloud1, Cloud2, Cloud3, Cloud3bis } from "./styles";
 import { motion } from "framer-motion";
 
 function Cloud() {
   return (
     <CloudContainer>
-      {/* <Cloud1 src={cloud1} className="cloud" alt="cloud1" />
-      <Cloud2 src={cloud2} className="cloud" alt="cloud2" /> */}
+      <motion.div
+        animate={{ x: -1000 }}
+        transition={{ loop: Infinity, ease: "linear", duration: 14 }}
+      >
+        <Cloud1 src={cloud1} className="cloud" alt="cloud1" />
+      </motion.div>
 
       <motion.div
         animate={{ x: -5000 }}
         transition={{ loop: Infinity, ease: "linear", times: 2, duration: 17 }}
       >
-        <Cloud3 src={cloud3} className="cloud" alt="cloud3" />
+        <Cloud2 src={cloud2} className="cloud" alt="cloud2" />
       </motion.div>
+
       <motion.div
         animate={{ x: -2000 }}
         transition={{ loop: Infinity, ease: "linear", duration: 15 }}
       >
-        <Cloud4 src={cloud4} className="cloud" alt="cloud4" />
+        <Cloud3 src={cloud3} className="cloud" alt="cloud3" />
       </motion.div>
 
       <motion.div
-        animate={{ x: -1000 }}
-        transition={{ loop: Infinity, ease: "linear", duration: 15 }}
+        animate={{ x: -2500 }}
+        transition={{ loop: Infinity, ease: "linear", duration: 16 }}
       >
-        <Cloud1 src={cloud1} className="cloud" alt="cloud1" />
+        <Cloud3bis src={cloud3} className="cloud" alt="cloud3bis" />
       </motion.div>
-      {/* <Cloud6 src={cloud6} className="cloud" alt="cloud6" />
-      <Cloud7 src={cloud7} className="cloud" alt="cloud7" />
-      <Cloud8 src={cloud8} className="cloud" alt="cloud8" />  */}
     </CloudContainer>
   );
 }
--- src/Cloud/styles.js
+++ src/Cloud/styles.js
@@ -4,64 +4,44 @@ import styled from "styled-components";
 export const CloudContainer = styled.div`
    {
     display: flex;
-    // flex-direction: column;
     justify-content: flex-end;
     padding: 100px 0;
   }
 `;
 
-// const Cloud1 = styled.img`
-//    {
-//     height: 300px;
-//     margin: 15px;
-//   }
-// `;
-// const Cloud2 = styled.img`
-//    {
-//     height: 300px;
-//     margin: 15px;
-//   }
-// `;
-export const Cloud3 = styled.img`
+export const Cloud1 = styled.img`
    {
     top: 10px;
+    position: relative;
+    height: 200px;
+    margin: 15px;
+  }
+`;
+
+export const Cloud2 = styled.img`
+   {
+    top: 300px;
     position: relative;
     height: 250px;
     margin: 15px;
   }
 `;
-export const Cloud4 = styled.img`
+
+export const Cloud3 = styled.img`
    {
-    top: 300px;
+    top: 500px;
     position: relative;
     height: 150px;
     margin: 15px;
   }
 `;
 
-export const Cloud1 = styled.img`
+export const Cloud3bis = styled.img`
    {
-    top: 500px;
+    top: 175px;
     position: relative;
-    height: 200px;
+    height: 250px;
     margin: 15px;
+    transform: scaleX(-1);
   }
 `;
-// const Cloud6 = styled.img`
-//    {
-//     height: 300px;
-//     margin: 15px;
-//   }
-// `;
-// const Cloud7 = styled.img`
-//    {
-//     height: 300px;
-//     margin: 15px;
-//   }
-// `;
-// const Cloud8 = styled.img`
-//    {
-//     height: 300px;
-//     margin: 15px;
-//   }
-// `;
